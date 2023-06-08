fbad6c03dc8baf7217f97b03691392928d7162e5
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2023-06-07 22:10:32 -0500

    Add game function and score calculations.

--- javascript.js
+++ javascript.js
@@ -3,35 +3,64 @@ function getComputerChoice () {
     let computerChoice;
     if (choiceNumber === 0) {
         computerChoice = "Rock";
-    }
-    else if (choiceNumber === 1) {
+    } else if (choiceNumber === 1) {
         computerChoice = "Paper";
-    }
-    else {
+    } else {
         computerChoice = "Scissors";
     }
 
     return computerChoice;
 }
 
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("Rock, Paper, or Scissors?")
+function playRound (computerSelection) {
+    let playerWin;
+    let playerSelection = prompt("Rock, Paper, or Scissors?")
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
-    console.log(playerSelection)
     let result;
 
     if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
-        result = "You Win! " + playerSelection + " beats " + computerSelection + "."
-    
+        result = "You Win! " + playerSelection + " beats " + computerSelection + ".";
+        playerWin = true;
     } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock") {
-        result = "You Lose! " + computerSelection + " beats " + playerSelection + "."
-
+        result = "You Lose! " + computerSelection + " beats " + playerSelection + ".";
+        playerWin = false;
     } else {
-        result = "Tie"
+        result = "Tie";
     }
 
-    return result
+    console.log(result)
+
+    return playerWin
 }
 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let ties = 0
+    let playerWin;
+    for (let i = 0; i < 5; i++) {
+       playerWin = playRound(getComputerChoice())
+       if (playerWin) {
+        playerScore += 1;
+       } else if (!playerWin) {
+        computerScore += 1;
+       } else {
+        ties += 1
+       }
+    }
+    console.log("Your score: " + playerScore)
+    console.log("Computer score: " + computerScore)
+    console.log("Ties: " + ties)
+
+    if (playerScore > computerScore) {
+        console.log("You win the game.")
+    } else if (playerScore < computerScore) {
+        console.log("You lose the game.")
+    } else {
+        console.log("You tied the game.")
+    }
+
+    game()
+}
 
