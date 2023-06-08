dae17c28ee99d95e76e50dc139006a2eedbf4e3c
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2023-06-07 20:13:27 -0500

    Create getComputerChoice function.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
+function getComputerChoice () {
+    let choiceNumber = Math.floor(Math.random()*3);
+    let computerChoice;
+    if (choiceNumber === 0) {
+        computerChoice = "Rock";
+    }
+    else if (choiceNumber === 1) {
+        computerChoice = "Paper";
+    }
+    else {
+        computerChoice = "Scissors";
+    }
 
+    return computerChoice
+    console.log(computerChoice)
+}
